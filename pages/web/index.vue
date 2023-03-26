a11c7f4670df98c369145ccb9e173a5c7d109344
龙权 <495502547@qq.com>
2023-03-26 15:23:16 +0800

    Feat: add web page

---
+++ pages/web/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <web-view :src="url">
+
+  </web-view>
+</template>
+
+<script>
+export default {
+  name: "index",
+  data() {
+    return {
+      url: 'https://www.baidu.com',
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
